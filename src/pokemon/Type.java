eb88a048bcc4f3b5ef4ec3161df13739eb3531f7
AdidharmaDewabrata <150336281+AdidharmaDewabrata@users.noreply.github.com>
2025-06-05 23:36:04 +0700

    updated

--- src/pokemon/Type.java
+++ src/pokemon/Type.java
@@ -1,5 +1,5 @@
 package pokemon;
 
 public enum Type {
-    FIRE, WATER, GRASS, NORMAL, ELECTRIC, FIGHTING, FLYING, FAIRY, BUG, DRAGON, ICE, GROUND // ... add more types
+    FIRE, WATER, GRASS, NORMAL, ELECTRIC, FIGHTING, FLYING, FAIRY, BUG, DRAGON, ICE, GROUND, HEAL // ... add more types
 }
